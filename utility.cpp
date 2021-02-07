4ca10b2992a47a8f9eaeb11a4a262210223b7462
MechaDragonX <theofficalrv@gmail.com>
2021-02-07 12:37:52 -0800

    Add documentation to toLower()

--- utility.cpp
+++ utility.cpp
@@ -2,10 +2,15 @@
 #include "utility.hpp"
 using namespace std;
 
-void Utility::toLower(string& original) {
-    for(int i = 0; i < original.length(); i++) {
-        if((int(original[i]) >= 65 && int(original[i]) <= 90)) {
-            original[i] = int(original[i]) + 32;
+/*
+    Parameters: String passed by reference
+    Returns: Nothing. Since the string is passed by reference, there is no need to return anything.
+    Description: Takes a string and modifies it so all characters are lowercase.
+*/
+void Utility::toLower(string& str) {
+    for(int i = 0; i < str.length(); i++) {
+        if((int(str[i]) >= 65 && int(str[i]) <= 90)) {
+            str[i] = int(str[i]) + 32;
         }
     }
 }
--- utility.hpp
+++ utility.hpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 namespace Utility {
+    /*
+        Parameters: String passed by reference
+        Returns: Nothing. Since the string is passed by reference, there is no need to return anything.
+        Description: Takes a string and modifies it so all characters are lowercase.
+    */
     void toLower(string& original);
 }
